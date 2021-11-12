6bf00af0171dbe5decb0761032607641c399030e
matheusfernand <theusf_47@hotmail.com>
2021-11-12 14:58:54 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,14 @@
 package com.nelioalves.workshopmongo.repository;
 
-import com.nelioalves.workshopmongo.domain.Post;
-import com.nelioalves.workshopmongo.domain.User;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import com.nelioalves.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
